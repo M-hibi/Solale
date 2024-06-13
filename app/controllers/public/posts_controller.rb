689c93d665909e4cc45ae2929ea13c0859961293
Marika Hibi <skifkate.0621@gmail.com>
2024-06-13 22:34:27 +0000

    tag_edit completed!

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -1,5 +1,5 @@
 class Public::PostsController < ApplicationController
-  
+
   def new
     @post = Post.new
   end
@@ -11,13 +11,13 @@ class Public::PostsController < ApplicationController
     notification.update(checked: true)
     end
   end
-  
+
   def show
     @post = Post.find(params[:id])
     @user = @post.user_id
     @comment = Comment.new
   end
-  
+
   def create
     @post = Post.new(post_params)
     tag_list = params[:post][:tag_names].split(',')
@@ -28,40 +28,40 @@ class Public::PostsController < ApplicationController
       render :new
     end
   end
-    
+
   def edit
     @post = Post.find(params[:id])
-    @tags = @post.tags.pluck(:name).join(',')
+    @tag_list = @post.tags.pluck(:name).join(',')
   end
 
   def update
     @post = Post.find(params[:id])
-    tags = params[:post][:tag_id]
+    tag_list = params[:post][:tag_names].split(',')
     if @post.update(post_params)
-      @post.update_tags(tags)
-      redirect_to request.referer
+      @post.save_tags(tag_list)
+      redirect_to post_path(@post.id), notice: "Solaleを編集しました"
     else
       render :edit
     end
   end
-  
+
   def destroy
   	post = Post.find(params[:id])
   	post.destroy
     redirect_to request.referer, notice: "Solaleとさよならしました"
   end
 
-  def search 
+  def search
       @tag = params[:tag]
       @posts = Post.page(params[:page]).joins(:tags).where('name LIKE ?',"#{@tag}")
   end
 
 
   private
-  
+
   def post_params
     params.require(:post).permit(:image).merge(user_id:current_user.id)
   end
-  
+
 
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -9,33 +9,24 @@ class Post < ApplicationRecord
   has_one :notification, as: :subject, dependent: :destroy
 
   validates :image, presence: true
-  
+
 
   def liked_by?(user)
     likes.exists?(user_id: user.id)
   end
-  
+
   def reported_by?(user)
     reports.exists?(user_id: user.id)
-  end  
-  
+  end
+
   def save_tags(savepost_tags)
+    if self.tags.present?
+      self.tags.delete(tags)
+    end
     savepost_tags.each do |new_name|
       post_tag = Tag.find_or_create_by(name: new_name)
       self.tags << post_tag
     end
   end
-  
-  def update_tags(latest_tags)
-    current_tags = self.tags.pluck(:name)
-    old_tags = current_tags - latest_tags
-    new_tags = latest_tags - current_tags
-    old_tags.each do |old_tag|
-      tag = self.tags.find_by(name: old_tag)
-      self.tags.delete(tag) if tag.present?
-    end
-    new_tags.each do |new_tag|
-      self.tags.find_or_create_by(name: new_tag)
-    end
-  end      
+
 end
