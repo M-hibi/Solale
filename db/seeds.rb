fc089daa810386333226f33b46d2b9493070636b
M-hibi <skifkate.0621@gmail.com>
2024-07-07 10:21:05 +0000

    SeedFileImput

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,100 @@
 Admin.create!(email: 'admin@admin.solale.jp', password: 'solalepass', password_confirmation: 'solalepass')
+
+olivia = User.find_or_create_by!(email: "olivia@example.com") do |user|
+  user.name = "Olivia"
+  user.password = "password"
+  user.password_confirmation = "password"
+  user.introduction = "Nice to meet you."
+end
+
+james = User.find_or_create_by!(email: "james@example.com") do |user|
+  user.name = "James"
+  user.password = "password"
+  user.password_confirmation = "password"
+  user.introduction = "Pleased to meet you."
+end
+
+lucas = User.find_or_create_by!(email: "lucas@example.com") do |user|
+  user.name = "Lucas"
+  user.password = "password"
+  user.password_confirmation = "password"
+  user.introduction = "It’s a pleasure to meet you."
+end
+
+Post.create!(
+  image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post1.jpg"), filename:"sample-post1.jpg"),
+  user: olivia
+)
+
+Post.create!(
+  image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post2.jpg"), filename:"sample-post2.jpg"),
+  user: james
+)
+
+Post.create!(
+  image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post3.jpg"), filename:"sample-post3.jpg"),
+  user: lucas
+)
+
+Post.create!(
+  image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post4.jpg"), filename:"sample-post4.jpg"),
+  user: olivia
+)
+
+Post.create!(
+  image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post5.jpg"), filename:"sample-post5.jpg"),
+  user: olivia
+)
+
+Post.create!(
+  image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post6.jpg"), filename:"sample-post6.jpg"),
+  user: olivia
+)
+
+Post.create!(
+  image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post7.jpg"), filename:"sample-post7.jpg"),
+  user: olivia
+)
+
+Post.create!(
+  image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post8.jpg"), filename:"sample-post8.jpg"),
+  user: lucas
+)
+
+
+Post.create!(
+  image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post9.jpg"), filename:"sample-post9.jpg"),
+  user: lucas
+)
+
+
+Post.create!(
+  image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post10.jpg"), filename:"sample-post10.jpg"),
+  user: lucas
+)
+
+
+Post.create!(
+  image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post11.jpg"), filename:"sample-post11.jpg"),
+  user: lucas
+)
+
+Post.create!(
+  image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post12.jpg"), filename:"sample-post12.jpg"),
+  user: james
+)
+
+Post.create!(
+  image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post13.jpg"), filename:"sample-post13.jpg"),
+  user: james
+)
+
+Post.create!(
+  image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post14.jpg"), filename:"sample-post14.jpg"),
+  user: james
+)
+
+Post.create!(
+  image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post15.jpg"), filename:"sample-post15.jpg"),
+  user: james
+)
